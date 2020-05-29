e45428e0043003c46d5357352b0c2ca12ab06c26
PabloRSantos <pablorsantos15@gmail.com>
2020-05-29 00:03:12 -0300

    reading add cards

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const card = document.querySelectorAll("#cards .cardObject")
-const dropZone = document.querySelectorAll("#cards .areaDrop") 
+var card = document.querySelectorAll("#cards .cardObject")
+var dropZone = document.querySelectorAll("#cards .areaDrop") 
 
-card.forEach( card => { 
+dropZone.forEach(dropZone => atribuirEventoDrop(dropZone))
+card.forEach(card => atribuirEventoCard(card))
+
+function atribuirEventoCard(card){
     card.addEventListener("dragstart", dragStart)
     card.addEventListener("drag", drag)
     card.addEventListener("dragend", dragEnd)
-})
+}
+
+function atribuirEventoDrop(dropZone){
+        dropZone.addEventListener("dragenter", dragEnter)
+        dropZone.addEventListener("dragleave", dragLeave)
+        dropZone.addEventListener("dragover", dragOver)
+}
 
 function dragStart(){
     this.classList.add("isDragging")
-    dropZone.forEach(dropZone => dropZone.style.backgroundColor = "rgba(255, 0, 0, 0.712)")
+    let dropZone2 = document.querySelectorAll("#cards .areaDrop")
+    dropZone2.forEach(dropZone => dropZone.style.backgroundColor = "rgba(255, 0, 0, 0.712)")
 }
 
 function drag (){
@@ -18,16 +28,9 @@ function drag (){
 
 function dragEnd(){
     this.classList.remove("isDragging")
-    dropZone.forEach(dropZone => dropZone.style.backgroundColor = "#284B63")
+    let dropZone2 = document.querySelectorAll("#cards .areaDrop")
+    dropZone2.forEach(dropZone => dropZone.style.backgroundColor = "#284B63")
 }
-
-dropZone.forEach(dropZone => {
-    dropZone.addEventListener("dragenter", dragEnter)
-    dropZone.addEventListener("dragleave", dragLeave)
-    dropZone.addEventListener("dragover", dragOver)
-    dropZone.addEventListener("drop", drop)
-})
-
 
 function dragEnter(){
     this.style.backgroundColor = "#4cd137bd"
@@ -39,6 +42,46 @@ function dragOver(){
 function dragLeave(){
     this.style.backgroundColor = "rgba(255, 0, 0, 0.712)"
 }
-function drop(){
 
-}
+const newCard = document.querySelector("#cards .addCard p")
+
+newCard.addEventListener("click", () => {
+    let new_card = document.createElement("div")
+    let top = document.createElement("div")
+    let h3 = document.createElement("h3")
+    let pTop = document.createElement("p")
+    let areaDrop = document.createElement("div")
+    let cardObject = document.createElement("div")
+    let status = document.createElement("div")
+    let pCard = document.createElement("p")
+    let cards = document.querySelector("#cards")
+
+    new_card.classList.add("card")
+    top.classList.add("top")
+    areaDrop.classList.add("areaDrop")
+    cardObject.classList.add("cardObject")
+    status.classList.add("status")
+    status.classList.add("red")
+    
+    h3.innerText = "Novo card"
+    pTop.innerText = "+"
+    pCard.innerText = "Tarefa"
+    cardObject.setAttribute("draggable", "true")
+
+    top.appendChild(h3)
+    top.appendChild(pTop)
+
+    cardObject.appendChild(status)
+    cardObject.appendChild(pCard)
+
+    areaDrop.appendChild(cardObject)
+
+    new_card.appendChild(top)
+    new_card.appendChild(areaDrop)
+
+    let addCard = document.querySelector("#cards .addCard")
+    cards.insertBefore(new_card, addCard)
+
+    atribuirEventoCard(cardObject)
+    atribuirEventoDrop(areaDrop)
+})
